8803e4b72cf09126b3561cb5f0194f08b35ff30c
yotamos5699 <m.lhafakot@gmail.com>
2022-12-05 10:38:51 +0200

    new users amir and yafit

--- src/Helpers/wizCloudUtiles/apiInterface/DocumentCreator.js
+++ src/Helpers/wizCloudUtiles/apiInterface/DocumentCreator.js
@@ -3,6 +3,8 @@
 const wizlib = require("wizcloud-api");
 const getCredential = require("../helpers/getCred");
 const realUserID = "6358f8717dd95eceee53eac3";
+const amir = "638dac1454f08b935ed4af2f";
+const yafit = "638dad0c54f08b935ed4af34";
 
 const tempKey =
   "23e54b4b3e541261140bdeb257538ba11c5104620e61217d5d6735a3c9361a5aac67a7f85278e4e53f3008598d8927f68e89e3e16147c194f96976bdf3075d55";
@@ -22,10 +24,10 @@ async function createDoc(docData, index, userID) {
   console.log(`creat doC startS doC num"${index}" data:\n ${JSON.stringify(docData)}`);
 
   let ID;
-  if (userID == realUserID) {
-    console.log("ofek is connected");
-    ID = realUserID;
-  } else ID = "1111";
+  if (userID == realUserID) ID = realUserID;
+  else if (userID == amir) ID = amir;
+  else if (userID == yafit) ID = yafit;
+  else ID = "1111";
   const { usserDbname, usserServerName, usserPrivetKey } = await getCredential.getCastumersCred(ID);
 
   let myDBname = usserDbname;
